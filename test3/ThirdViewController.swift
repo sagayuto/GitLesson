1d5737a7444d07f8bbaacbda42079c0807d65adc
佐賀祐人 <sagayuuto@yuto.local>
2019-05-23 21:04:12 +0900

    お試し

---
+++ test3/ThirdViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ThirdViewController.swift
+//  test3
+//
+//  Created by 佐賀祐人 on 2019/05/23.
+//  Copyright © 2019 net.sample. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+class ThirdViewController: BaseViewController {
+    
+}
+
+// MARK: - Life cycle
+extension ThirdViewController {
+    override func loadView() {
+        super.loadView()
+        fndjksnfkdnsf
+        fkdfklndsa.
+        fdnsfnds.
+        
+        fdsflka/
+        
+        
+        
+    }
+}
+
+// MARK: - Protocol
+extension ThirdViewController {
+    
+}
+
+// MARK: - method
+extension ThirdViewController {
+    
+}
+
